2c1f521c13ce0944d0b3851501c3ec2268eaec61
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-02-19 04:05:53 +0530

    Time: 1 ms (99.99%), Space: 48.4 MB (12.47%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,22 +1,26 @@
-class Solution {
-    public int[] countBits(int n) {
-        // int[] arr = new int[n+1];
-        // for(int i=0; i<=n; i++){
-        //     arr[i] = count(i);
-        // }
-        // return arr;.
+// class Solution {
+//     public int[] countBits(int n) {
+//         int[] arr = new int[n+1];
+//         for(int i=0; i<=n; i++){
+//             arr[i] = count(i);
+//         }
+//         return arr;
+//     }
+//     static int count(int i){
+//         int count =0;
+//         while(i != 0){
+//             count += i &1; 
+//             i = i>>1;
+//         }
+//         return count;
+//     }
+// }
+class Solution{
+    public int[] countBits(int n){
         int[] f = new int[n + 1];
         for (int i=1; i<=n; i++) {
             f[i] = f[i >> 1] + (i & 1); // if i =8 ..f[8>>1] = f[4] = f[2]
         }
         return f;
     }
-    // static int count(int i){
-    //     int count =0;
-    //     while(i != 0){
-    //         count += i &1; 
-    //         i = i>>1;
-    //     }
-    //     return count;
-    // }
 }
