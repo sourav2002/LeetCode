f522c552b9b3ec73646c92b4086aa969589d8d19
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-02-18 20:31:36 +0530

    Time: 1 ms (92.56%), Space: 42.4 MB (17.80%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -3,8 +3,8 @@ public class Solution {
     public int reverseBits(int n) {
         int result = 0;
         for (int i = 0; i < 32; i++) {
-            result <<= 1;
-            result |= n&1;
+            result <<= 1; 
+            result += n&1;
             n >>>= 1;   // CATCH: must do unsigned shift
         }
     return result;
