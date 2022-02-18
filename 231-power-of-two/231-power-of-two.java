b62c00e31f6e5f0c04ed60a772e0f6c3d8d04f36
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-02-19 03:11:36 +0530

    Time: 28 ms (12.00%), Space: 45.4 MB (5.58%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        // Size of an integer is assumed to be 32 bits
+        if(n == 1){
+            return true;
+        }
+        String s = "";
+        for (int i = 31; i >= 0; i--) {
+            int k = n >> i;
+            if ((k & 1) > 0)
+                s+= 1;
+            else
+                s+=0;
+        }
+        String result = removeLeadingZeroes(s);
+        int len = result.length();
+        System.out.println(s);
+        System.out.println(Math.pow(2, len -1));
+        if(Math.pow(2, len-1) == n){
+            return true;
+        }
+        
+        return false;
+    }
+       public static String removeLeadingZeroes(String str) {
+      String strPattern = "^0+(?!$)";
+      str = str.replaceAll(strPattern, "");
+      return str;
+   }
+}
