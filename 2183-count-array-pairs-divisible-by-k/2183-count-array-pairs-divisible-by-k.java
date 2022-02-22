79ffb7ae1a53399ee61b7efd3579d2ae1186e104
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-02-23 01:22:49 +0530

    Time: 500 ms (60.65%), Space: 55.1 MB (90.59%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
@@ -2,17 +2,24 @@ class Solution {
     public long countPairs(int[] nums, int k) {
         long result = 0;
         Map<Integer, Integer> gcdMap = new HashMap<>();
+        
         for (int i = 0; i < nums.length; i++) {
+            
             int gcd = gcd(nums[i], k);
+            
             for (int num : gcdMap.keySet()) {
+                
                 if ((long) gcd * num % k == 0) { // Need to convert gcd(i) * gcd(j) to long, sad java
                     result += gcdMap.get(num);
                 }
             }
+            
             gcdMap.put(gcd, gcdMap.getOrDefault(gcd, 0) + 1);
         }
+        
         return result;
     }
+    
     private int gcd(int x, int y) {
         if (y == 0) {
             return x;
