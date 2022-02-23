64f7e0cd794fee185e8512cc54a9843f0f1f2875
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-02-23 22:21:37 +0530

    Time: 27 ms (11.75%), Space: 43.6 MB (32.22%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,48 +1,13 @@
 class Solution {
     public String reverseWords(String s) {
-        
-        StringBuffer ans = new StringBuffer();
-        int i = 0;
-        while(s.charAt(i) == ' '){
-            i++;
-        }
-        s =s.substring(i, s.length());
-        if(s.length() == 1){
-            return s;
-        }
         System.out.println(s);
-        int n = s.length() -1 ;
-        while( n != 0){
-            System.out.println("initially : "+n);
-            StringBuffer word = new StringBuffer();
-            while( n > 0 && s.charAt(n) == ' '){
-                n--;
-            }
-            System.out.println("if contain any space then after removing, n = " + n);
-            while(n > 0 && s.charAt(n) != ' '){
-                word.append(s.charAt(n));
-                n--;
-            }
-            
-            if(n == 0 && s.charAt(0) != ' '){
-                word.append(s.charAt(0));
-            }
-            
-            System.out.println("after adding char in word, n = "+ n);
-            System.out.println("before reverse = "+ word);
-            word.reverse();
-            System.out.println("after reverse = "+ word);
-            ans.append(word);
-            if(n !=0 ){
-               ans.append(' '); 
-            }
-                        
-            System.out.println("after appending with ans = "+ ans);
-            System.out.println();
-            System.out.println();
-            System.out.println();
-        }
-        System.out.println("final ans is = "+ ans.toString());
-        return ans.toString();
+        String[] words = s.trim().split(" +"); // 1st
+        System.out.println(Arrays.toString(words));
+        Collections.reverse(Arrays.asList(words)); // 2nd
+        System.out.println(Arrays.asList(words));
+        String ans = String.join(" ", words); // 3rd
+        System.out.println(ans);
+        System.out.println();
+        return ans;
     }
 }
