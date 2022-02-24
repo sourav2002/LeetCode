dcf04c36975b7326e96df47ae1523a632c909faf
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-02-24 13:23:56 +0530

    Time: 727 ms (13.99%), Space: 47.1 MB (16.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -3,12 +3,7 @@ class Solution {
         s = s.toLowerCase();
         s = s.replaceAll("[^a-zA-Z0-9]","");
         System.out.println(s);
-        System.out.println(s.length());
-        for(int i = 0; i< s.length(); i++){
-            if(s.charAt(i) != s.charAt(s.length() - i - 1)){
-                return false;
-            }
-        }
-        return true;
+        String rev = new StringBuffer(s).reverse().toString();
+        return s.equals(rev);
     }
 }
