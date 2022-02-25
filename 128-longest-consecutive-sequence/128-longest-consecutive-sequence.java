f09c6a3d22d485290cde3356abdeb0d156058f12
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-02-26 00:50:28 +0530

    Time: 506 ms (23.51%), Space: 148.3 MB (12.05%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> hs = new HashSet<>();
+        for(int num : nums){
+            hs.add(num);
+        }
+        
+        int longestStreak = 0;
+        for(int n : nums){
+            if(!hs.contains(n-1)){ // check if the current number is the starting no. of consecutive seq or not 
+                int currentNum = n;
+                int currentStreak = 1;
+                
+                while(hs.contains(currentNum +1)){
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+}
+/*
+ put all numbers of array in a hashmap because it is easy to check if an array has x element or not
+ now run a for loop and check if the number has not any previous value because we need consecutive sequence and it    requires 1st element to be smallest in the subset of consecutive seq.
+ 
+ check if hashmap contains( current num + 1) if yes then update currentnum and increment current streak of sequence 
+*/
