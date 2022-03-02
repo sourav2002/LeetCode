e12e499172e97c9aa60488d4a740980a62281581
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-02 20:03:35 +0530

    Time: 30 ms (85.15%), Space: 48.2 MB (26.03%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,46 +1,57 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-//         if(needle.length() == 0){
-//             return 0;
-//         }
+        if(needle.length() == 0){
+            return 0;
+        }
         
-//         // using z-index string algorithm
-//         String nst = needle+"$"+haystack;
-//         System.out.println(nst);
+        // using z-index string algorithm
+        String nst = needle+"$"+haystack;
+        System.out.println(nst);
         
-//         int[] z = new int[nst.length()];
-//         z = calculateZ(nst);
+        int[] z = new int[nst.length()];
+        z = calculateZ(nst);
         
-//         System.out.println(Arrays.toString(z));
+        System.out.println(Arrays.toString(z));
         
-//         for(int i=0; i< z.length; i++){
-//             if(z[i] == needle.length()){
-//                 return i- needle.length() -1;
-//             }
-//         }
+        for(int i=0; i< z.length; i++){
+            if(z[i] == needle.length()){
+                return i- needle.length() -1;
+            }
+        }
         
-//         return -1;
-//     }
+        return -1;
+    }
     
-//     public static int[] calculateZ(String s){
-//         int[] z = new int[s.length()];
-//         char[] carr = s.toCharArray();
+    public static int[] calculateZ(String s){
+        int[] z = new int[s.length()];
+        char[] carr = s.toCharArray();
         
-//         int left = 0, right = 0;
-//         for(int k = 1; k < s.length(); k++){
-//             left = right = k;
+        int left = 0, right = 0;
+        for(int k = 1; k < s.length(); k++){
             
-//             while(right < s.length() && carr[right] == carr[right - left]){
-//                 right++;
-//             }
-            
-//             z[k] = right - left;
-//             right--;
-//         }
-//         return z;
-        
-        // tle for this solution
-        
-        return haystack.indexOf(needle);
+            if(k > right){
+                left = right = k;
+                while(right < s.length() && carr[right] == carr[right - left]){
+                  right++;
+                }
+                z[k] = right - left;
+                right--;
+            }else{
+                int k1 = k - left;
+                if(z[k1] < right - k +1){
+                    z[k] = z[k1];
+                }else{
+                    left = k;
+                    while(right < s.length() && carr[right] == carr[right- left]){
+                        right++;
+                    }
+                    z[k] = right - left;
+                    right--;
+                }  
+            }
+        }
+        return z;
+                
+        // return haystack.indexOf(needle);
     }
 }
