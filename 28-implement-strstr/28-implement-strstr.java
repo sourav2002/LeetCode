35a7a5609b7e7a200983c58f1ca0455a07e45593
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-02 19:43:27 +0530

    Time: 471 ms (63.00%), Space: 42.9 MB (45.16%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,5 +1,46 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+//         if(needle.length() == 0){
+//             return 0;
+//         }
+        
+//         // using z-index string algorithm
+//         String nst = needle+"$"+haystack;
+//         System.out.println(nst);
+        
+//         int[] z = new int[nst.length()];
+//         z = calculateZ(nst);
+        
+//         System.out.println(Arrays.toString(z));
+        
+//         for(int i=0; i< z.length; i++){
+//             if(z[i] == needle.length()){
+//                 return i- needle.length() -1;
+//             }
+//         }
+        
+//         return -1;
+//     }
+    
+//     public static int[] calculateZ(String s){
+//         int[] z = new int[s.length()];
+//         char[] carr = s.toCharArray();
+        
+//         int left = 0, right = 0;
+//         for(int k = 1; k < s.length(); k++){
+//             left = right = k;
+            
+//             while(right < s.length() && carr[right] == carr[right - left]){
+//                 right++;
+//             }
+            
+//             z[k] = right - left;
+//             right--;
+//         }
+//         return z;
+        
+        // tle for this solution
+        
         return haystack.indexOf(needle);
     }
 }
