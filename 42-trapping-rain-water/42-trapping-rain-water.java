dfb88e879aad579300f45321493ebebeb79b201e
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-09 18:05:20 +0530

    Time: 13 ms (10.49%), Space: 47.9 MB (5.80%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,5 +1,29 @@
 class Solution {
     public int trap(int[] height) {
+        Stack<Integer> x= new Stack<>();
+        int distance,top,b_height,ans=0;
+        
+        for(int i=0;i<height.length;i++){
+            while(!x.empty() && height[i]>height[x.peek()]){
+                top=x.pop();
+                if(x.empty()){
+                    //distance=0;
+                    break;
+                }
+                distance=i-x.peek()-1;
+                b_height=Math.min(height[i],height[x.peek()]) - height[top];
+                ans=ans+(distance*b_height);
+              
+            }
+            x.push(i);
+        }
+        return ans;
+    }
+}
+
+/*
+class Solution {
+    public int trap(int[] height) {
         if(height.length==0){
             return 0;
         }
@@ -29,3 +53,38 @@ class Solution {
         return water;
     }
 }
+*/
+
+/*
+class Solution {
+    public int trap(int[] height) {
+        if(height.length==0){
+            return 0;
+        }
+        int maxht = 0,index=0;
+        for(int i=0;i<height.length;i++){
+            if(height[i]>maxht){
+                maxht=height[i];
+                index=i;
+            }
+        }
+        int localmax=height[0];
+        int globalmax=height[index];
+        int water=0;
+        for(int i=1;i<index;i++){
+            if(height[i]<localmax){
+                water+=localmax-height[i];
+            }
+            localmax=Math.max(localmax,height[i]);
+        }
+         localmax=height[height.length-1];
+        for(int i=height.length-2;i>index;i--){
+            if(height[i]<localmax){
+                water+=localmax-height[i];
+            }
+            localmax=Math.max(localmax,height[i]);
+        }
+        return water;
+    }
+}
+*/
