84b59b2d31f3a422c860c8c98108b462545c9a64
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-13 17:50:22 +0530

    Time: 13 ms (82.67%), Space: 41.8 MB (93.97%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -1,4 +1,18 @@
 class Solution {
+        public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c == 'c') {
+                if (stack.isEmpty() || stack.pop() != 'b') return false;
+                if (stack.isEmpty() || stack.pop() != 'a') return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+    
+    /*
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         
@@ -21,4 +35,5 @@ class Solution {
         }
         return true;
     }
+    */
 }
