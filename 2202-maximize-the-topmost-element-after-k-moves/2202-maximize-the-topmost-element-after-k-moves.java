6428428e34e7b3d54045e64251ddb3b06ab63301
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-13 14:48:34 +0530

    Time: 1 ms (100.00%), Space: 85.4 MB (50.00%) - LeetHub

---
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumTop(int[] arr, int k) {
+        int n = arr.length;
+        
+        if(n == 1 && k%2 == 1) return -1;
+        
+        int ans = 0;
+        if(k > n){
+            for(int i : arr) ans = Math.max(ans, i);
+            return ans;
+        }
+        
+        else if(k == n){
+            for(int i =0; i< k-1; i++) ans = Math.max(ans, arr[i]); 
+            return ans;
+        }
+        else if(k < n){
+            for(int i =0; i< k-1; i++) ans = Math.max(ans, arr[i]); 
+            return Math.max(ans, arr[k]);
+        }
+        return ans;
+    }
+}
