a965f6ec54f7ab327c20c37754f47dd95cf67427
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-16 13:13:40 +0530

    Time: 4 ms (66.35%), Space: 45.1 MB (43.80%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,14 +1,36 @@
 class Solution {
-        public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer> stack = new Stack<>();
-        int i = 0;
-        for (int x : pushed) {
-            stack.push(x);
-            while (!stack.empty() && stack.peek() == popped[i]) {
-                stack.pop();
-                i++;
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st= new Stack<>();
+        
+        int popind = 0;
+        
+        for(int i : pushed){
+            st.push(i);
+            while(popind < popped.length && !st.isEmpty()  && popped[popind] == st.peek()){
+                st.pop();
+                popind++;
             }
         }
-        return stack.empty();
+
+        if(st.isEmpty()){
+            return true;
+        }
+        return false;
     }
 }
+
+
+// class Solution {
+//         public boolean validateStackSequences(int[] pushed, int[] popped) {
+//         Stack<Integer> stack = new Stack<>();
+//         int i = 0;
+//         for (int x : pushed) {
+//             stack.push(x);
+//             while (!stack.empty() && stack.peek() == popped[i]) {
+//                 stack.pop();
+//                 i++;
+//             }
+//         }
+//         return stack.empty();
+//     }
+// }
