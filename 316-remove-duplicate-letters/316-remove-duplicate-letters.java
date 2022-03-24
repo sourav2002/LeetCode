ee4c30585f3de4625bd251acedc0325752c572af
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-24 12:17:54 +0530

    Time: 5 ms (52.50%), Space: 42.7 MB (61.37%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -24,3 +24,16 @@ class Solution {
         return sb.reverse().toString();
     }
 }
+
+/*
+take a stack to let char in and out according to our requirment
+take a "lastIndex" array to store the last index of every char
+take a boolean array "seen", whenever we put a char in stack mark it seen
+
+    1) take a stack, and put the 1st char of string in stack and also mark that char as seen
+    2) for next char, if char is already seen before then skip further steps. else follow further steps.
+    2) for next char, if current char is smaller then char at top of the stack(st.peek()), then remove char at top
+    from the stack(only if char at top of stack is not last occurence of that char ) and also make that char as             unseen(false)
+    3) put the current char in our stack only if it is unseen (if seen[ch] == true) then continue; and mark it seen
+    4) pop the stack values and append in stringbuilder, and return reverse of stringBuilder
+*/
