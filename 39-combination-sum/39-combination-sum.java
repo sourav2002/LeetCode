67b052d2ce319365114f48be565e8fc83bbbdac0
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-24 18:01:03 +0530

    Time: 5 ms (65.10%), Space: 45.1 MB (37.30%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -6,6 +6,7 @@ class Solution {
     }
     
     public static void backtrack(int[] arr, int target, List<List<Integer>> ans, ArrayList<Integer> list, int i,int sum){
+        if(sum > target) return;
         if(i == arr.length){
             if(sum == target){
                 ans.add(new ArrayList<>(list));
@@ -13,14 +14,18 @@ class Solution {
             return;
         }
         
-        if(sum < target){ // bcz we are taking same number multiple time
+        // if(sum < target){ // bcz we are taking same number multiple time
             list.add(arr[i]);
             sum += arr[i];
             backtrack(arr,target, ans,list,i, sum);      // <-- here       
             list.remove(list.size()-1); 
             sum -= arr[i];
-        }
+        // }
 
         backtrack(arr, target, ans,list,i+1,sum);
     }
 }
+/*
+if we dont use if(sum < target ) condition then everytime backtrack for same value is called then it is gonna add sum += arr[i] and it will never stop means stackoverflow. so we only take same value until we get the target 
+
+*/
