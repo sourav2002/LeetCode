c01f1ec0922a55916aec3d08d5edfb76e53df8c0
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-27 16:00:07 +0530

    Time: 9 ms (50.00%), Space: 100 MB (50.00%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
@@ -4,6 +4,7 @@ class Solution {
         
         for(int i=0; i< nums.length-1; i++){
             if((i-c) % 2 == 0 && nums[i] == nums[i+1]){  
+                // i-c to shift index to left c  times bcz we deleted c times
                 c++;
             }
         }
@@ -13,3 +14,13 @@ class Solution {
     }
     
 }
+
+/*
+1). every time we find a duplicate we will increment ans by 1.
+
+2). thus to check the second condition we will subtract the ans value from the index as we are shifting the element to the left.
+
+3). for the last edge case we will check the remaining size after removing ans element from the vector if the result is odd we have to remove 1 more element else just return the ans.
+Code :-
+
+*/
