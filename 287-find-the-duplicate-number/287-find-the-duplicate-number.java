bdcfc1f99951f17f54fd32f5786049ba3fd614c5
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-03-29 22:38:00 +0530

    Time: 5 ms (86.43%), Space: 76.2 MB (29.05%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        
         int slow = nums[0];
         int fast = nums[0];
+        
+        // use do while loop to avoid 1st iteration where slow and fast is already equals.
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
@@ -12,7 +15,29 @@ class Solution {
             slow = nums[slow];
             fast = nums[fast];
         }
-        return slow;
+        return fast;
         
+        
+        
+// there is one extra num so find actual sum of without extra num, means  n-1
+// 1,3,4,2,2  n = 5
+// after n-1
+// 1+2+3+4 
+// assumed = 10
+// actual sum = 12
+// actual - assumed = 2;
+// return 2
+        /*
+        int n = nums.length-1;
+        long assumedSum = n*(n+1)/2;
+        long sum = 0;
+        
+        for(int num : nums){
+            sum += num;
+        }
+        
+        Long diff = sum - assumedSum;
+        return diff.intValue();
+        */
     }
 }
