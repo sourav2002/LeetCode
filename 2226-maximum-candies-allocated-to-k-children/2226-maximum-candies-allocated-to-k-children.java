e699b40008d4ca757ef38b2c03a73062372bc7c5
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-03 15:18:50 +0530

    Time: 236 ms (33.33%), Space: 78.2 MB (66.67%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maximumCandies(int[] c, long k) {
+        
+        long max = 0;
+        for(int i: c){
+            max += i;
+        }
+        if(k > max) return 0;
+        
+        long low = 1;
+        long high = max;
+        long res = -1;
+        
+        while(low <= high){
+            long mid = low+high >>>1;            
+            if(isPossible(mid, c, k)){
+                res = mid;
+                low = mid +1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return (int)res;
+    }
+    
+    public boolean isPossible(long mid, int[] arr, long k){
+        
+        long split = 0;
+        for(int i=0; i< arr.length; i++){
+            split += arr[i]/ mid;
+        }
+        
+        return split >= k; 
+    }
+}
