9de6ec9e6e14e976f99067ba5e3001cbb29e4278
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-12 00:17:47 +0530

    Time: 9 ms (52.71%), Space: 54.6 MB (55.04%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,11 +1,13 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int temp = -1;
-        List<List<Integer>> list = new ArrayList();
+        
+        List<List<Integer>> list = new ArrayList(); // final list
         int i =-1;
         int n = grid.length;
         int m = grid[0].length;
         
+        // n*m is total no. of elements, let suppose n=3, m=3, total = 9
+        // if k is also 9 then no need to change the grid
         k = k % (n*m);
         
         List<Integer> row = new ArrayList();
@@ -22,24 +24,31 @@ class Solution {
             return list;
         }
         
+        // remove k elements from the last of  row list and add them in helper list
         ArrayList<Integer> helper = new ArrayList();
         int rm = k;
         while(rm-->0){
             helper.add(row.remove(row.size()-1));
         }
+        
+        // now remove all elements from helper and add in front of row list
         while(helper.size() > 0){
             row.add(0, helper.remove(0));
         }
         
-        List<Integer> fin = new ArrayList();
+        
+        // find length of horizonally elements (length of any row)
+        // add that amout of element in temp then add temp in final list
+        // every time we hit j % m (where m is the horizontal length) reinitialized temp
+        List<Integer> temp = new ArrayList();
         for(int j = 0; j< row.size(); j++){
             if(j % m == 0){
-                if(j != 0) list.add(fin);
-                fin = new ArrayList();
+                if(j != 0) list.add(temp);
+                temp = new ArrayList();
             }
-            fin.add(row.get(j));
+            temp.add(row.get(j));
         }
-        list.add(fin);
+        list.add(temp);
         return list;
     }
 }
