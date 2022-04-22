a3f1c6a937c85564aec1ea7051b9c82cab964bdc
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-22 17:33:14 +0530

    Time: 3 ms (14.00%), Space: 42.7 MB (51.30%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,73 @@
+/*
+// morris traversal (create connection of root's left extreme right node with root's right node)
+class Solution {
+
+    public void flatten(TreeNode root) {
+        TreeNode cur = root;
+        if(root == null) return;
+        while (cur != null) {
+            if (cur.left == null) {
+                cur = cur.right;
+            } else {
+                TreeNode thread = cur.left;
+                while (thread.right != null) {
+                    thread = thread.right;
+                }
+
+                thread.right = cur.right;
+                cur.right = cur.left;
+                cur.left = null;
+                cur = cur.right;
+            }
+        }
+    }
+}
+*/
+
+
+/*
+// DFS traversal( right -> Left -> root)
+class Solution {
+
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        
+        flatten(root.right);
+        flatten(root.left);
+        
+        root.right = prev;
+        root.left = null;
+        prev = root;
+        
+    }
+}
+*/
+
+
+
+// (Linear traversal using stack)
+class Solution {
+
+    public void flatten(TreeNode root) {
+        if(root == null)return;
+        Stack<TreeNode> st = new Stack();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode cur = st.pop();
+            if(cur.right != null){
+                st.push(cur.right);
+            }
+            if(cur.left != null){
+                st.push(cur.left);
+            }
+            
+            if(!st.isEmpty()){
+                cur.right = st.peek();
+                cur.left = null;
+            }
+            
+        }
+    }
+}
+
