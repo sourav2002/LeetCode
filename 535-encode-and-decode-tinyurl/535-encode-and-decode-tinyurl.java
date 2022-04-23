b8ac320ca934e9380505fc8caeed7844b7741ae3
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-23 17:46:25 +0530

    Time: 15 ms (12.14%), Space: 43.1 MB (67.17%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,16 +1,33 @@
 public class Codec {
 
-    Map<Integer, String> map = new HashMap();
+    Map<String, String> map = new HashMap();
     String host = "http://tinyurl.com/";
+    
+    public String hashing(String longUrl){
+        String hashcode = "";
+        for(int i=0; i<6; i++){
+            int random = (int)Math.random() % 3;
+            
+            if(random == 0) hashcode += '0' + Math.random() % 10;
+            else if(random == 1) hashcode += 'a' + Math.random() % 26;
+            else  hashcode += 'A' + Math.random() % 26;
+        }
+        
+        if(map.containsKey(hashcode)){
+            return hashing(longUrl);
+        }else{
+            map.put(hashcode, longUrl);
+            return hashcode;
+        }
+    }
 
     public String encode(String longUrl) {
-      int key = longUrl.hashCode();
-      map.put(key, longUrl);
-      return host+key;
+        String code = hashing(longUrl);
+        return host+code;
     }
 
     public String decode(String shortUrl) {
-      int key = Integer.parseInt(shortUrl.replace(host,""));
-      return map.get(key);
+        String hashcode = shortUrl.replace(host, "");
+        return map.get(hashcode);
     }
 }
