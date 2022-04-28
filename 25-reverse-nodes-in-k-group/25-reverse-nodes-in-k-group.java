3b7279c273929cf282825ff1a54605a9dd50e064
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-28 14:41:05 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (91.18%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -1,14 +1,6 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+/*
 class Solution {
+
     public ListNode reverseKGroup(ListNode head, int k) {
         int l = 0;
         ListNode cur = head;
@@ -20,15 +12,56 @@ class Solution {
             return head;
         }
         cur = head;
-        ListNode node = null;
+        ListNode prev = null;
         for (int i = 0; i < k; i++) {
             ListNode nxt = cur.next;
-            cur.next = node;
-            node = cur;
+            cur.next = prev;
+            prev = cur;
             cur = nxt;
         }
         head.next = reverseKGroup(cur, k);
-        return node;    
+        return prev;
     }
+}
+*/
 
+class Solution{
+    public ListNode reverseKGroup(ListNode head, int k){
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode pointer = dummy;
+        while(pointer != null){
+            // check if remaining nodes are greater or equal to k or not
+            ListNode check = pointer;
+            for(int i=0; i<k && check != null; i++){
+                check = check.next;
+            }
+            
+            if(check == null) break;
+            
+            // else we know that we have k nodes. we will start from the first node
+            ListNode curr = pointer.next;
+            ListNode prev = null, nxt = null;
+            for(int i=0; i<k; i++){
+                nxt = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nxt;
+            }
+            
+            // after reversing
+            // connect the current tail(or old head) with curr node. because curr is pointing to k+1th node(or we can say head of next window)
+            
+            ListNode tail = pointer.next; // pointer.next was starting/beginning node 
+            tail.next = curr; // connected successfully 
+            pointer.next = prev; // it is connecting last tail with next head
+            pointer = tail;
+            // update pointer 
+// After one iteration, pointer was at 1 and prev is at 4(new head of 2nd window).
+// so 1 should point to 4 to create 2->1->4->3
+// That's why we are using this line pointer.next = prev
+        }
+        return dummy.next;
+    }
 }
