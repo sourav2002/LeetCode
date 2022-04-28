94034710530e6bb02d73fed52260076d3f2f9188
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-28 18:15:01 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (86.26%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -14,22 +14,62 @@ class Solution {
             return head;
         }
         
-        ListNode temp = head;
-        int len = 1; // l= 1 bcz we will stop temp on last node 
-        while(temp.next != null){
-            temp = temp.next;
+//         ListNode temp = head;
+//         int len = 1; // l= 1 bcz we will stop temp on last node
+//         while(temp.next != null){
+//             temp = temp.next;
+//             len++;
+//         }
+        
+//         temp.next = head; // form cycle
+//         k = k%len; // in case if k is greater then size of list
+//         int i = len - k; // temp is at last node of list. Move it i times.
+//         for(int loop = i; loop > 0; loop--){
+//             temp = temp.next;
+//         }
+        
+//         ListNode newH = temp.next; // now break the cycle but before breaking store next pointer bcz its gonna be //new head
+//         temp.next = null;
+//         return newH;
+        
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode slow = dummy, fast=head;
+        
+        int len = 0;
+        while(fast != null){
+            fast = fast.next;
             len++;
         }
-        
-        temp.next = head; // form cycle
-        k = k%len; // in case if k is greater then size of list
-        int i = len - k; // temp is at last node of list. Move it i times.
-        for(int loop = i; loop > 0; loop--){
-            temp = temp.next;
+
+        k = k% len;
+        if( k == len || k == 0){
+            return head;
         }
         
-        ListNode newH = temp.next; // now break the cycle but before breaking store next pointer bcz its gonna be //new head
-        temp.next = null;
-        return newH;
+        fast = dummy;
+        while(k-->0 && fast != null){
+            fast = fast.next;
+        }
+        while(fast != null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        ListNode newHead = slow.next;
+        slow.next = null;
+        if(fast !=null){
+            fast.next = head;
+            return newHead;
+        }else{
+            ListNode temp = newHead;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = head;
+            return newHead;
+        }
     }
 }
