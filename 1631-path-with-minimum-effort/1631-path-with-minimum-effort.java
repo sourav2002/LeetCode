c9833cad01316f0dbfeac8558a2e8e945ddb744c
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-29 02:22:39 +0530

    Time: 71 ms (65.63%), Space: 53.5 MB (72.48%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,4 +1,43 @@
 class Solution {
+
+    public int minimumEffortPath(int[][] heights) {
+        int low = 0;
+        int high = 1_000_000;
+        int m = heights.length, n = heights[0].length;
+        int[][] dirs = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            boolean[][] visited = new boolean[m][n];
+            if (minimumEffortDFS(heights, mid, 0, 0, m, n, dirs, visited)) {
+                ans = mid;
+                high = mid - 1;
+            } else low = mid + 1;
+        }
+
+        return ans;
+    }
+
+    // O(m*n)
+    public boolean minimumEffortDFS(int[][] heights, int mid, int x, int y, int m, int n, int[][] dirs, boolean[][] visited) {
+        if (x == m - 1 && y == n - 1) return true;
+
+        visited[x][y] = true;
+
+        for (int[] dir : dirs) {
+            int row = x + dir[0];
+            int col = y + dir[1];
+
+            if (row == -1 || col == -1 || row == m || col == n || visited[row][col] || Math.abs(heights[x][y] - heights[row][col]) > mid) continue;
+
+            if (minimumEffortDFS(heights, mid, row, col, m, n, dirs, visited)) return true;
+        }
+        return false;
+    }
+}
+/*
+class Solution {
     int[] dx = new int[] {0, 1, 0, -1};
     int[] dy = new int[] {1, 0, -1, 0};
     public int minimumEffortPath(int[][] heights) {
@@ -36,3 +75,4 @@ class Solution {
         return false;
     }
 }
+*/
