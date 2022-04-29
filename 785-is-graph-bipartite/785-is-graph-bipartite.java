f5fd998d8e8effbb183e2891b081046568d84452
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-29 23:04:02 +0530

    Time: 1 ms (83.33%), Space: 53.7 MB (52.29%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -0,0 +1,39 @@
+/*
+Our goal is trying to use two colors to color the graph and see if there are any adjacent nodes having the same color.
+Initialize a color[] array for each node. Here are three states for colors[] array:
+0: Haven't been colored yet.
+1: Blue.
+-1: Red.
+For each node,
+
+If it hasn't been colored, use a color to color it. Then use the other color to color all its adjacent nodes (DFS).
+If it has been colored, check if the current color is the same as the color that is going to be used to color it. (Please forgive my english... Hope you can understand it.)
+*/
+
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] colors = new int[n];			
+				
+        for (int i = 0; i < n; i++) {              
+            //This graph might be a disconnected graph. So check each unvisited node.
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }       
+        colors[node] = color;       
+        for (int next : graph[node]) {
+            if (!validColor(graph, colors, -color, next)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
