2edc2849d7b9eaee950dfcbaf8cb498823920917
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-04-30 22:58:57 +0530

    Time: 35 ms (100.00%), Space: 130.7 MB (50.00%) - LeetHub

---
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
@@ -0,0 +1,40 @@
+class Solution
+{
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls)
+    {
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        char[][] grid= new char[m][n];
+        for(int[] wall : walls)
+        {
+            int x = wall[0], y = wall[1];
+            grid[x][y] = 'W';
+        }
+        for(int[] guard : guards)
+        {
+            int x = guard[0], y = guard[1];
+            grid[x][y] = 'G';
+        }
+        for(int[] point : guards)
+        {
+            for(int dir[] : dirs)
+            {
+                int x = point[0] + dir[0];
+                int y = point[1] + dir[1];
+                while(x >= 0 && y >= 0 && x < m && y < n && grid[x][y] != 'G' && grid[x][y] != 'W')
+                {
+                    grid[x][y] = 'P';
+                    x += dir[0];
+                    y += dir[1];
+                }
+            }
+        }
+        
+        int count = 0;
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                if(grid[i][j] != 'G' && grid[i][j] != 'W' && grid[i][j] != 'P')
+                    count++;
+        
+        return count;
+    }
+}
