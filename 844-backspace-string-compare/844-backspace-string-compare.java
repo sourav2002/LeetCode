14869e7cae5d4cecab6f657024440626ac289ee6
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-05-01 10:40:04 +0530

    Time: 1 ms (87.56%), Space: 41.9 MB (63.23%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,24 +1,48 @@
-class Solution {
-    
+/*class Solution {
+
     private String getString(String str) {
-        int n=str.length(), count=0;
-        String result="";
-        for(int i=n-1; i>=0; i--) {
-            char ch=str.charAt(i);
-            if(ch=='#') 
-                count++;
-            else {
-                if(count>0)
-                    count--;
-                else {
-                    result+=ch;
-                }                     
+        int n = str.length(), count = 0;
+        String result = "";
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            if (ch == '#') count++; else {
+                if (count > 0) count--; else {
+                    result += ch;
+                }
             }
         }
         return result;
     }
-    
+
     public boolean backspaceCompare(String S, String T) {
         return getString(S).equals(getString(T));
+    }
+}
+*/
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (S.charAt(i) == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
+            }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (T.charAt(j) == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
+        }
+        return true;
     }
 }
