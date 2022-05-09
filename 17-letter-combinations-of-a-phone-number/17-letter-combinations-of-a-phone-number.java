a1ca19de7e71ed4ff8e933ae9397853002aef5f9
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-05-09 17:30:56 +0530

    Time: 10 ms (13.14%), Space: 43.4 MB (6.53%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,4 +1,23 @@
 class Solution {
+
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new LinkedList<String>();
+        if (digits.isEmpty()) return ans;
+        String[] mapping = new String[] { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        ans.add(0, "");
+        for (int i = 0; i < digits.length(); i++) {
+            int x = digits.charAt(i) - '0';
+            int size = ans.size(); // number of nodes/strings already in the queue
+            for (int k = 1; k <= size; k++) {
+                String t = ans.remove(0);
+                for (char s : mapping[x].toCharArray()) ans.add(t + s);
+            }
+        }
+        return ans;
+    }
+}
+/*
+class Solution {
     public List<String> letterCombinations(String digits) {
         
         List<String> list = new ArrayList<>();
@@ -33,3 +52,4 @@ class Solution {
         }
     }
 }
+*/
