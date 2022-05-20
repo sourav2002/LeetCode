98ecec840f8385c8102c70d180a03a84e4d30ddc
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-05-21 02:17:26 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (81.57%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,3 +1,29 @@
+// dp tabulation
+class Solution{
+    int mod = (int) (2 * 1000000000);
+    
+    public int uniquePathsWithObstacles(int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i=0; i< m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = (up + left) % mod;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
+// dp memoization
+/*
 class Solution {
     int mod = (int) (2 * 1000000000);
     public int uniquePathsWithObstacles(int[][] grid) {
@@ -15,3 +41,4 @@ class Solution {
         return dp[i][j] = (up + left) % mod;
     }
 }
+*/
