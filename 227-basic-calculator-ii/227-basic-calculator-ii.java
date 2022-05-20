03b27e020355926b684e0d8d92b8ec216f1c33ce
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-05-20 18:46:45 +0530

    Time: 7 ms (96.06%), Space: 42.1 MB (95.96%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int calculate(String s) {
+        int sum = 0;
+        int tempSum = 0;
+        int num = 0;
+        char lastSign = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) num = num * 10 + c - '0';
+            if (i == s.length() - 1 || !Character.isDigit(c) && c != ' ') { // i == s.len -1 for calculating last digit of the string
+                switch (lastSign) {
+                    case '+':
+                        sum += tempSum;
+                        tempSum = num;
+                        break;
+                    case '-':
+                        sum += tempSum;
+                        tempSum = -num;
+                        break;
+                    case '*':
+                        tempSum *= num;
+                        break;
+                    case '/':
+                        tempSum /= num;
+                        break;
+                }
+                lastSign = c;
+                num = 0;
+            }
+        }
+        sum += tempSum;
+        return sum;
+    }
+}
+
+// store current sum if the sign is * or /. if the sign is + or -, then add current sum into result.
