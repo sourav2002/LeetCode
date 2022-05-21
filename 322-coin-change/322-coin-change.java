349ddce5984a71581733fe0f643ef06f7093b5a5
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-05-22 02:06:07 +0530

    Time: 142 ms (10.22%), Space: 74.3 MB (6.91%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,21 +1,60 @@
 // we can't go greedy if we want minimum no. of coins.
-
+/*
 class Solution {
 
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
-        int[] dp = new int[amount+1];
+        int[] dp = new int[amount + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i=0;i<=amount;i++){
-            for(int coin : coins){
-                if(i - coin >= 0 && dp[i-coin] != Integer.MAX_VALUE){
-                    dp[i] = Math.min(dp[i], dp[i-coin] +1);
+        for (int i = 0; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        
-        if(dp[amount] == Integer.MAX_VALUE) return -1;
+
+        if (dp[amount] == Integer.MAX_VALUE) return -1;
         return dp[amount];
+    }
+}
+*/
+
+// bfs solution
+class Solution {
+
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[amount + 1];
+
+        queue.offer(amount);
+        visited[amount] = true;
+        int currLevel = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            System.out.println(queue);
+            for (int i = 0; i < size; i++) {
+                int curr = queue.poll();
+
+                for (int coin : coins) {
+                    int child = curr - coin;
+                    if (child == 0) {
+                        return currLevel;
+                    } else if (child > 0 && !visited[child]) {
+                        queue.add(child);
+                        visited[child] = true;
+                    }
+                }
+            }
+            currLevel++;
+        }
+
+        return -1;
     }
 }
