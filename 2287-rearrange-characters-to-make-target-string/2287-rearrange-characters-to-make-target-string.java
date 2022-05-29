53be0d89d9a14ccfc74bf4d840a5afaae83fcaf6
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-05-29 14:04:08 +0530

    Time: 1 ms (100.00%), Space: 42 MB (50.00%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -1,27 +1,19 @@
 class Solution {
     public int rearrangeCharacters(String s, String target) {
-        int[] freqt = new int[26];
-        for(int i=0; i<target.length(); i++){
-            int ind = target.charAt(i) - 'a';
-            freqt[ind]++;
+        int[] sc = new int[26];
+        int[] tc = new int[26];
+        for(int i=0;i<s.length();i++){
+            sc[s.charAt(i)-'a']++;
         }
-        
-        int[] freqs = new int[26];
-        for(int i=0; i< s.length();i++){
-            int ind = s.charAt(i) - 'a';
-            freqs[ind]++;
+        for(int i=0;i<target.length();i++){
+            tc[target.charAt(i)-'a']++;
         }
-        int c = 0;
-        
-        while(true){
-            for(int i=0; i<26;i++){
-                if(freqs[i] >= freqt[i]){
-                    freqs[i] = freqs[i] - freqt[i];
-                }else{
-                    return c;
-                }
+        int ans = s.length();
+        for(int i=0;i<26;i++){
+            if(tc[i]!=0) {
+                ans = Math.min(ans,sc[i]/tc[i]);
             }
-            c++;
         }
+        return ans;
     }
 }
