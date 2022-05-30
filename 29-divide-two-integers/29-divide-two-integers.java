d87f0834a15df29aa3cc16a73d7c74c352669198
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-05-30 18:57:15 +0530

    Time: 2 ms (62.23%), Space: 42.3 MB (5.86%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -1,12 +1,19 @@
 class Solution {
 
-    public int divide(int A, int B) {
-        if (A == 1 << 31 && B == -1) return (1 << 31) - 1;
-        int a = Math.abs(A), b = Math.abs(B), res = 0;
-        for (int x = 31; x >= 0; x--) if ((a >>> x) - b >= 0) {
-            res += 1 << x;
-            a -= b << x;
+    public int divide(int dividend, int divisor) {
+        if(dividend == 1<< 31 && divisor == -1)return Integer.MAX_VALUE;
+        boolean sign = (dividend >= 0) == (divisor >= 0) ? true:false;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int result = 0;
+        while(dividend - divisor >= 0){
+            int count=0;
+            while(dividend - (divisor << 1 << count) >= 0){
+                count ++;
+           }
+            result += 1<< count;
+            dividend -= divisor << count;
         }
-        return (A > 0) == (B > 0) ? res : -res;
+        return sign ? result: -result;
     }
 }
