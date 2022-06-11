cc9dcea6c9c78aeec943196f785484d91ea1d852
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-12 02:05:19 +0530

    Time: 7 ms (69.09%), Space: 95.2 MB (66.98%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -3,6 +3,30 @@ class Solution {
         int target = -x;
         for(int i: nums) target += i;
         if(target == 0) return nums.length;
+        if(target < 0) return -1;
+        int sum = 0;
+        int res = -1;
+        int i = 0, left = 0;
+        for(i = 0; i< nums.length; i++){
+            sum += nums[i];
+            while(sum > target){
+                sum -= nums[left];
+                left++;
+            }
+            if(sum == target){
+                res = Math.max(res, i - left + 1);
+            }
+        }
+        return res == -1 ? -1 : nums.length - res;
+    }
+}
+
+/*
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int target = -x;
+        for(int i: nums) target += i;
+        if(target == 0) return nums.length;
         Map<Integer, Integer> map = new HashMap();
         map.put(0, -1);
         int res = -1;
@@ -18,3 +42,4 @@ class Solution {
         return res == -1 ? -1 : nums.length - res;
     }
 }
+*/
