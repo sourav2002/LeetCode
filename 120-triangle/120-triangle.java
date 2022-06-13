16dd551abd96848ad3fc653ce5f686a8befc28d4
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-13 17:23:27 +0530

    Time: 4 ms (61.51%), Space: 44.2 MB (41.06%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,17 +1,83 @@
+// class Solution {
+
+//     //Recursive
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         if (triangle == null || triangle.size() == 0) return 0;
+
+//         return dfs(0, 0, triangle);
+//     }
+
+//     public int dfs(int row, int pos, List<List<Integer>> triangle) {
+//         //Out of bounds so just return sum previous value must be leaf node
+//         if (row + 1 >= triangle.size()) return triangle.get(row).get(pos);
+
+//         return triangle.get(row).get(pos) + Math.min(dfs(row + 1, pos, triangle), dfs(row + 1, pos + 1, triangle));
+//     }
+// }
+
+// //Top down
+// class Solution {
+
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         if (triangle == null || triangle.size() == 0) return 0;
+//         Integer[][] cache = new Integer[triangle.size()][triangle.size()];
+//         return dfs(0, 0, triangle, cache);
+//     }
+
+//     int dfs(int row, int pos, List<List<Integer>> triangle, Integer[][] cache) {
+//         //Out of bounds so just return sum previous value must be leaf node
+//         if (row + 1 >= triangle.size()) {
+//             return triangle.get(row).get(pos);
+//         }
+//         if (cache[row][pos] != null) return cache[row][pos];
+//         cache[row][pos] = triangle.get(row).get(pos) + Math.min(dfs(row + 1, pos, triangle, cache), dfs(row + 1, pos + 1, triangle, cache));
+//         return cache[row][pos];
+//     }
+// }
+
+// //Bottom up
+// class Solution {
+
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int[][] dp = new int[triangle.size()][triangle.size()];
+//         //Remember base case is just returning leaf nodes
+//         for (int i = 0; i < triangle.size(); i++) {
+//             dp[triangle.size() - 1][i] = triangle.get(triangle.size() - 1).get(i);
+//         }
+//         for (int row = triangle.size() - 2; row >= 0; row--) {
+//             for (int pos = 0; pos < triangle.get(row).size(); pos++) {
+//                 dp[row][pos] = triangle.get(row).get(pos) + Math.min(dp[row + 1][pos + 1], dp[row + 1][pos]);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
+
+
+//Bottom up O(n) space complexity.
+
 class Solution {
-    public int minimumTotal(List<List<Integer>> tri) {
-        Integer[][] dp = new Integer[tri.size()][tri.size()];
-         return  tri.get(0).get(0) + solve(tri,1,0, dp);
-    }
-    
-    public int solve(List<List<Integer>> tri, int i, int j, Integer[][] dp){
-        if(i == tri.size()){
-            return 0;
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size()];
+        int[] dp1 = new int[triangle.size()];
+        //Remember base case is just returning leaf nodes
+        for (int i = 0; i < triangle.size(); i++) {
+            dp[i] = triangle.get(triangle.size() - 1).get(i);
         }
-        if(dp[i][j] != null) return dp[i][j];
-        int left = tri.get(i).get(j) + solve(tri, i+1, j, dp);
-        int right = tri.get(i).get(j+1) + solve(tri, i+1, j+1, dp);
-        dp[i][j] =  Math.min(left, right);
-        return dp[i][j];
+        for (int row = triangle.size() - 2; row >= 0; row--) {
+            for (int pos = 0; pos < triangle.get(row).size(); pos++) {
+                dp1[pos] = triangle.get(row).get(pos) + Math.min(dp[pos + 1], dp[pos]);
+            }
+            dp = dp1;
+        }
+        return dp[0];
     }
 }
+
+
+/*
+we are having 2  choices. Either choose the value at index (i,j) or the value at index(i,j+1)
+in recursion, it is possible that for one of the recursive call is on the same track which is previously solved by one the the recursion calls.
+So for that repetitive calculation, use dp memoization.
+*/
