86c470bb7631d5b5a4794eaab07a8c7ac27f2954
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-15 20:56:02 +0530

    Time: 6 ms (99.13%), Space: 45.4 MB (89.42%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,26 +1,22 @@
+/*
+1) to check if 2 strings are anagram, we can sort both of them and then put in hashmap sorted string as key and original string as value.
+2) we can use hashing to get frequency of char in a string. If frequency mapping of 2 strings are same, then we can put them in hashmap to hasing array as key and list of having same freq set strings.
+*/
+
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> list = new ArrayList();
         if(strs.length == 0){
             return list;
         }
-    
         Map<String, ArrayList<String>> map = new HashMap();
-        
         for(String s : strs){
             String ns = sort(s);
-            if(map.containsKey(ns)){
-                map.get(ns).add(s);
-            }else{
-                map.put(ns, new ArrayList<String>());
-                map.get(ns).add(s);
-            }
+            if(!map.containsKey(ns)) map.put(ns, new ArrayList<String>());
+            map.get(ns).add(s);
         }
-        System.out.println(map);
-        for(Map.Entry<String, ArrayList<String>> entry : map.entrySet()){
-            list.add(entry.getValue());
-        }
-        return list;
+        return new ArrayList<>(map.values());
         
     }
     
