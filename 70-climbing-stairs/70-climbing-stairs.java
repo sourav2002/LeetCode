d29644bd2fca1c3df84416c9d5ec8f6417763ea1
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-18 23:23:10 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (15.79%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,3 +1,19 @@
+class Solution{
+    public int climbStairs(int n){
+        if(n <= 2) return n;
+        int a = 1;
+        int b = 2;
+        int res = 0;
+        for(int i = 3; i <= n; i++){
+            res = a + b;
+            a = b;
+            b = res;
+        }
+        return res;
+    }
+}
+
+/*
 class Solution {
     int c = 0;
     int num;
@@ -15,4 +31,4 @@ class Solution {
         if(dp[n] != 0) return dp[n];
         return dp[n] = dfs(n-1, dp) + dfs(n-2, dp);
     }
-}
+}*/
