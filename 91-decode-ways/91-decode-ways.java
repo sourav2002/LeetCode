4e0df69d11f24fc8b7b8aeb191eb6ec5c2e0d3a3
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-18 12:57:03 -0700

    Time: 1 ms (97.97%), Space: 41.7 MB (78.38%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,16 +1,13 @@
-class Solution {
-    int i;
-    int[] dp;
+class Solution {    
     public int numDecodings(String s) {
         char[] arr = s.toCharArray();
-        dp = new int[arr.length+1];
+        int[] dp = new int[arr.length+1];
         Arrays.fill(dp, -1);
         if(arr[0] == '0') return 0;
-        i = arr.length-1;
-        return dfs(arr, i);
+        return dfs(arr, arr.length-1, dp);
     }
     
-    public int dfs(char[] arr, int i){
+    public int dfs(char[] arr, int i, int[] dp){
         if(i <= 0){
             return 1;
         }
@@ -19,14 +16,14 @@ class Solution {
         int x = 0;
         int y = 0;
         if(arr[i] != '0'){
-           x = dfs(arr, i-1);
+           x = dfs(arr, i-1, dp);
         }
         if(arr[i-1] != '0'){
             if(arr[i-1] == '2' && arr[i] > '6' || arr[i-1] > '2'){
                 dp[i] = x;
                 return x;
             }
-            y = dfs(arr, i-2);
+            y = dfs(arr, i-2, dp);
         }
         return dp[i] = x+y;
     }
