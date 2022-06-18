077637967f6867234e99a3833a4ec3e1e37fffe3
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-18 16:01:19 +0530

    Time: 1 ms (76.97%), Space: 44.4 MB (7.65%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -13,6 +13,52 @@
  *     }
  * }
  */
+
+class Solution {
+
+    enum State {
+        CAMERA,
+        MONITORED,
+        NOT_MONITORED
+    }
+
+    private static class Status {
+        int cameras;
+        State state;
+
+        Status(int cameras, State state) {
+            this.cameras = cameras;
+            this.state = state;
+        }
+    }
+
+    public int minCameraCover(TreeNode root) {
+        Status status = getStatus(root);
+        if (status.state == State.NOT_MONITORED) {
+            status.cameras++;
+        }
+        return status.cameras;
+    }
+
+    private Status getStatus(TreeNode node) {
+        if (node == null) return new Status(0, State.MONITORED);
+
+        Status left = getStatus(node.left);
+        Status right = getStatus(node.right);
+
+        Status curr = new Status(left.cameras + right.cameras, State.NOT_MONITORED);
+
+        if (left.state == State.NOT_MONITORED || right.state == State.NOT_MONITORED) {
+            curr.cameras++;
+            curr.state = State.CAMERA;
+        } else if (left.state == State.CAMERA || right.state == State.CAMERA) {
+            curr.state = State.MONITORED;
+        }
+
+        return curr;
+    }
+}
+/*
 class Solution {
     int res = 0;
 
@@ -22,7 +68,8 @@ class Solution {
 
     public int dfs(TreeNode root) {
         if (root == null) return 2;
-        int left = dfs(root.left), right = dfs(root.right);
+        int left = dfs(root.left);
+        int right = dfs(root.right);
         if (left == 0 || right == 0) {
             res++;
             return 1;
@@ -30,3 +77,4 @@ class Solution {
         return left == 1 || right == 1 ? 2 : 0;
     }
 }
+*/
