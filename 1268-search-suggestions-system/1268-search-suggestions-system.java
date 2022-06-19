37d3ec5237036f41ec52a85a2e9ded4318492a04
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-19 14:50:44 -0700

    Time: 64 ms (49.41%), Space: 77.2 MB (29.82%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,3 +1,46 @@
+// using trie data structure
+class Solution {
+
+    class Trie {
+        Trie[] sub = new Trie[26];
+        LinkedList<String> suggestion = new LinkedList<>();
+    }
+
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Trie root = new Trie();
+        for (String p : products) { // build Trie.
+            insert(p, root); // insert a product into Trie.
+        }
+        return search(searchWord, root);
+    }
+
+    private void insert(String p, Trie root) {
+        Trie t = root;
+        for (char c : p.toCharArray()) { // insert current product into Trie.
+            if (t.sub[c - 'a'] == null) t.sub[c - 'a'] = new Trie();
+            t = t.sub[c - 'a'];
+            t.suggestion.offer(p); // put products with same prefix into suggestion list.
+            Collections.sort(t.suggestion);
+            if (
+                t.suggestion.size() > 3
+            ) t.suggestion.pollLast(); // maintain 3 lexicographically minimum strings.
+        }
+    }
+
+    private List<List<String>> search(String searchWord, Trie root) {
+        List<List<String>> ans = new ArrayList<>();
+        for (char c : searchWord.toCharArray()) { // search product.
+            if (
+                root != null
+            ) root = root.sub[c - 'a']; // if there exist products with current prefix.
+            ans.add(root == null ? Arrays.asList() : root.suggestion); // add it if there exist products with current prefix.
+        }
+        return ans;
+    }
+}
+// brute force solution
+// time complexity = n * m * o (where n is the size of searchWOrd len and m is size of products array and o is the size of each string in array)
+/*
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         List<List<String>> list = new ArrayList();
@@ -23,3 +66,4 @@ class Solution {
         return list;
     }
 }
+*/
