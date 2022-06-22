97f99b609bb69f91bff358e5be81890fdb54dd65
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-06-22 01:30:44 -0700

    Time: 16 ms (97.69%), Space: 80 MB (16.27%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,13 +1,38 @@
 class Solution {
 
-    public int furthestBuilding(int[] A, int bricks, int ladders) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < A.length - 1; i++) {
-            int d = A[i + 1] - A[i];
-            if (d > 0) pq.add(d);
-            if (pq.size() > ladders) bricks -= pq.poll();
-            if (bricks < 0) return i;
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int bricksUsed = 0;
+        int n = heights.length;
+        int i = 0;
+        for (; i < n - 1; i++) {
+            int diff = heights[i + 1] - heights[i];
+
+            // If current height is higher, we don't have to use
+            // bricks or ladder.
+            if (diff <= 0) {
+                continue;
+            }
+
+            // We have to use either brick or ladder.
+            if (diff + bricksUsed <= bricks) {
+                // Prefer bricks because we can later change from
+                // bricks to ladder as we will see below.
+                bricksUsed += diff;
+                pq.add(diff);
+            } else if (ladders > 0) {
+                // If even bricks are insufficient, check ladders.
+                if (!pq.isEmpty() && pq.peek() > diff) {
+                    bricksUsed += diff - pq.peek();
+                    pq.poll();
+                    pq.add(diff);
+                }
+                ladders--;
+            }
+            // Unfortunately, it's not possible to go ahead any further.
+            else break;
         }
-        return A.length - 1;
+        // This is the max we can go ahead.
+        return i;
     }
 }
