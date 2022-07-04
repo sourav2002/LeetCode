1293e7bf62936645433c22b457b98e46c2cf2938
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-07-04 05:29:31 -0700

    Time: 3 ms (100.00%), Space: 41.5 MB (80.00%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
@@ -1,6 +1,22 @@
 class Solution {
 
     public int peopleAwareOfSecret(int n, int delay, int forget) {
+        long dp[] = new long[n + 1], mod = (long) 1e9 + 7, share = 0, res = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i){
+            dp[i] = share = (share + dp[Math.max(i - delay, 0)] - dp[Math.max(i - forget, 0)] + mod) % mod;
+        }
+        
+        for (int i = n - forget + 1; i <= n; ++i){
+            res = (res + dp[i]) % mod;
+        }
+        return (int) res;
+    }
+}
+/*
+class Solution {
+
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
         int mod = 1000000000 + 7;
         List<pair> list = new ArrayList();
         list.add(new pair(1 + delay, 1 + forget, 1));
@@ -40,3 +56,4 @@ class Solution {
         }
     }
 }
+*/
