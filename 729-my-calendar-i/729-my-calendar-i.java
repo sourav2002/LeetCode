568bf351125cba5d9081819e58364c4406a4fb87
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-08-03 12:37:39 -0700

    Time: 109 ms (22.96%), Space: 54.1 MB (78.49%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -0,0 +1,9 @@
+class MyCalendar {
+    private List<int[]> books = new ArrayList<>();
+    public boolean book(int start, int end) {
+        for (int[] b : books)
+            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
+        books.add(new int[]{ start, end });
+        return true;
+    }
+}
