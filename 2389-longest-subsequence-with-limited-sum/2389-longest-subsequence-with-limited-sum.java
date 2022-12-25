51d1cc8ad7cd242d67d1e3e2179c538437561ced
Sourav Saini <60310993+sourav2002@users.noreply.github.com>
2022-12-25 17:03:46 +0530

    Time: 4 ms (99.87%), Space: 42.8 MB (88.86%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,30 +1,13 @@
 class Solution {
-    public int[] answerQueries(int[] nums, int[] queries) {
-        int []ans = new int[queries.length];
-        int ind = 0;
-        Arrays.sort(nums);
-        for (int q : queries){
-            Queue<Integer> que = new LinkedList();
-            int sum = 0;
-            int max = 0;
-            // System.out.println("-----------for value = " + q +"--------------");
-            for(int i : nums){
-                while(!que.isEmpty() && sum + i > q){
-                    sum -= que.remove();
-                    // System.out.println("sum decre = "+ sum);
-                }
-                if(sum < 0) sum = 0;
-                if(sum + i <= q){
-                    sum += i;
-                    que.add(i);
-                }
-                max = Math.max(max, que.size());
-                // System.out.println("max = "+ max);
-                // System.out.println("sum = "+ sum);
-                
-            }
-            ans[ind++] = max;
+
+    public int[] answerQueries(int[] A, int[] queries) {
+        Arrays.sort(A);
+        int n = A.length, m = queries.length, res[] = new int[m];
+        for (int i = 1; i < n; ++i) A[i] += A[i - 1];
+        for (int i = 0; i < m; ++i) {
+            int j = Arrays.binarySearch(A, queries[i]);
+            res[i] = Math.abs(j + 1);
         }
-        return ans;
+        return res;
     }
 }
